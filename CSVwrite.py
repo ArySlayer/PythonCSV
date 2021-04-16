144f0897ffe809ee15a61ae3a95bd3d122029c3d
ArySlayer <aryav1049@gmail.com>
2021-04-16 09:22:20 +0200

    working

--- CSVwrite.py
+++ CSVwrite.py
@@ -1,8 +1,7 @@
 import csv
 
 with open('NewCSV.csv', mode='w', newline='') as employee_file:
-employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-employee_writer.writerow(['Name', 'Dept', 'Birth-month'])
-employee_writer.writerow(['John Smith', 'Accounting', 'November'])
-employee_writer.writerow(['Erica Meyers', 'IT', 'March'])
+    employee_writer.writerow(['John Smith', 'Accounting', 'November'])
+    employee_writer.writerow(['Erica Meyers', 'IT', 'March'])
